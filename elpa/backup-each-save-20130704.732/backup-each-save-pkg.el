95b42a8004e7ee35ca6319cf266f95a60973bc73
syamajala <syamajala@gmail.com>
2016-03-27 07:38:17 -0400

    Remove backup each save.

--- elpa/backup-each-save-20130704.732/backup-each-save-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "backup-each-save" "20130704.732" "backup each savepoint of a file" 'nil)
---
+++ elpa/seq-2.13/seq-autoloads.el
@@ -0,0 +1,16 @@
+;;; seq-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("seq-24.el" "seq-25.el" "seq-pkg.el" "seq.el")
+;;;;;;  (22262 59309 433240 548000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; seq-autoloads.el ends here
