188a60fbcf3c2b521ae0d944657399830759c3ac
Venkata Yamajala <syamajala@gmail.com>
2017-03-21 04:11:18 -0400

    Update packages.

--- elpa/window-layout-20150716.2207/window-layout-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; window-layout-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("window-layout.el") (22134 18938 346774
-;;;;;;  770000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; window-layout-autoloads.el ends here
